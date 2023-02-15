0f8240e76dc0e798e31af8ad98ca442dc006cff3
Laura_Arias <kokolizo22@outlook.com>
2023-02-15 15:08:03 -0500

    +buttons when new card created and comments

--- behavior.js
+++ behavior.js
@@ -1,5 +1,8 @@
 
 /*I would like to seperate each element into its own bucket*/
+/*This first part focuses on the functions of the buttons and such*/
+
+
 
 /*Deletes the task when the delete button is clicked*/
 
@@ -24,6 +27,8 @@ function deleteTask(){
 	}
 }
 
+/*-----------*/
+/*This part is primarily for creating*/
 
 
 /*Creates new Task by creating a new card and appending it to
@@ -75,11 +80,11 @@ function createNewCardDivBody(){
 	newBodyDiv.setAttribute("class", "card-body");
 
 	const input = createNewInputDiv();
-	const description = createDescriptionDiv();
+	const options = createButtonGroupDiv()
 	const deleteButton = createDeleteButtonDiv();
 
 	newBodyDiv.appendChild(input);
-	newBodyDiv.appendChild(description);
+	newBodyDiv.appendChild(options);
 	newBodyDiv.appendChild(deleteButton);
 
 	return newBodyDiv;
@@ -98,9 +103,11 @@ function createNewInputDiv(){
 
 	const checkBox = createNewCheckboxDiv();
 	const title = createTitle();
+	const priorityButton = createPriorityBtn();
 
 	inputDiv.appendChild(checkBox);
 	inputDiv.appendChild(title);
+	inputDiv.appendChild(priorityButton);
 
 	return inputDiv;
 }
@@ -153,6 +160,25 @@ function createTitle(){
 	return newTitle;
 }
 
+/*Creating a button responsible for prioratizing the task
+Attributes set: 
+- class = "btn btn-outline-secondary"
+- type = "button"
+- id = "priorityButton"
+- title = "Prioratize this task?"
+*/
+
+function createPriorityBtn(){
+	const newPriorityButton = document.createElement("button");
+	newPriorityButton.setAttribute("class", "btn btn-outline");
+	newPriorityButton.setAttribute("type", "button");
+	newPriorityButton.setAttribute("id", "priorityButton");
+	newPriorityButton.setAttribute("title", "Prioratize this task?");
+	newPriorityButton.innerHTML = "Prioratize?";
+
+	return newPriorityButton;
+}
+
 /*creating the seperate div for task description
 Attribute set:
  - class = "input-group"
@@ -183,6 +209,86 @@ function createDescriptionTextArea(){
 	
 	return descrptionAreaText;
 }
+
+/*Creating the div that will hold all the buttons
+Attributes set:
+- id = "button-divs"
+- class = "btn-group"
+- role = "group"
+
+and appends the buttons to the div
+*/
+
+function createButtonGroupDiv(){
+	const newGroupBtnDiv = document.createElement("div");
+	newGroupBtnDiv.setAttribute("class", "btn-group");
+	newGroupBtnDiv.setAttribute("role", "group");
+	newGroupBtnDiv.setAttribute("id", "button-divs");
+
+	newGroupBtnDiv.appendChild(createAddSubBtn());
+
+	newGroupBtnDiv.appendChild(createAddDateBtn());
+
+	newGroupBtnDiv.appendChild(createAddDescriptionBtn());
+
+	return newGroupBtnDiv;
+
+
+}
+
+/*creating all of the buttons that will be put into the div
+Add subtask:
+Attributes set:
+- type = "button"
+- class = "btn btn-primary"
+- id = "add-subtask-btn"
+
+Add Due Date:
+Attributes set:
+- type = "button"
+- class = "btn btn-dark"
+= id = "addDueDate"
+
+Add Task Description
+Attributes set:
+- type = "button"
+- class = "btn"
+- id = "add-description"
+
+*/
+
+function createAddSubBtn(){
+	const subtaskBtn = document.createElement("button");
+	subtaskBtn.setAttribute("type", "button");
+	subtaskBtn.setAttribute("class", "btn btn-primary");
+	subtaskBtn.setAttribute("id", "add-subtask-btn");
+	subtaskBtn.innerHTML = "Add Subtask";
+
+	return subtaskBtn;
+}
+
+function createAddDateBtn(){
+	const dueDateBtn = document.createElement("button");
+	dueDateBtn.setAttribute("type", "button");
+	dueDateBtn.setAttribute("class", "btn btn-dark");
+	dueDateBtn.setAttribute("id", "addDueDate");
+	dueDateBtn.innerHTML = "Add Due Date";
+
+	return dueDateBtn;
+
+}
+
+function createAddDescriptionBtn(){
+	const descriptionBtn = document.createElement("button");
+	descriptionBtn.setAttribute("type", "button");
+	descriptionBtn.setAttribute("class", "btn");
+	descriptionBtn.setAttribute("id", "add-description");
+	descriptionBtn.innerHTML = "Add Task Description";
+
+	return descriptionBtn;
+
+}
+
 
 /*Creating the div that will hold the delete button and appending the actual button to it
 Atttributes set:
